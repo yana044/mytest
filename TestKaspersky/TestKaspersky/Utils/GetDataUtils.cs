5bd272b5437fd028bbccfff0845be79099e167c8
NatalyaYakimovich <ya.natalya04@gmail.com>
2022-09-27 17:59:08 +0300

    add proj

---
+++ TestKaspersky/TestKaspersky/Utils/GetDataUtils.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace TestKaspersky
+{
+    public class GetDataUtils
+    {
+        public static string GetConfigData(string key)
+        {
+            using StreamReader reader = File.OpenText("configData.json");
+            {
+                JsonTextReader jsonTextReader = new JsonTextReader(reader);
+                JObject jsonObject = (JObject)JToken.ReadFrom(jsonTextReader);
+                string value = jsonObject[key].ToString();
+                reader.Close();
+                return value;
+            }
+        }
+    }
+}
