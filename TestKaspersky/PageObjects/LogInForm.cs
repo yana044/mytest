611ac7441d074a451f2575af6d4183b4cb5a1c49
yana044 <ya.natalya044@yandex.by>
2022-09-28 10:57:20 +0300

    good2

--- TestKaspersky/PageObjects/LogInForm.cs
+++ TestKaspersky/PageObjects/LogInForm.cs
@@ -20,6 +20,7 @@ namespace TestKaspersky
             emailTextBox.Type(username);
             passwordTextBox.State.WaitForEnabled();
             passwordTextBox.Type(password);
+            singinbtn.State.WaitForClickable();
             singinbtn.ClickAndWait();
         }
     }
