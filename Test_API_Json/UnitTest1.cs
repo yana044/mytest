fe44196845f3ffad2c08cabd9a366dd567bf0ec3
yana044 <ya.natalya044@yandex.by>
2022-09-28 11:51:07 +0300

    add logs

--- Test_API_Json/UnitTest1.cs
+++ Test_API_Json/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Aquality.Selenium.Browsers;
 using NUnit.Framework;
 using System.Linq;
 using Test_API_Json.Models;
@@ -11,43 +12,44 @@ namespace Test_API_Json
         [Test]
         public void Test1()
         {
-            //ResponseModel<PostModel> allposts = APIAppRequest<PostModel>.GetData();
-            //int statuscodeposts = allposts.StatusCode;
-            //int expStatusforposts = (int)Status—ode.Step1;
-            //Assert.IsTrue(statuscodeposts == expStatusforposts, $"Status code is NOT {expStatusforposts}");
+            AqualityServices.Logger.Info("Step 1: Send GET Request to get all posts");
+            ResponseModel<PostModel> allposts = APIAppRequest<PostModel>.GetData();
+            int statuscodeposts = allposts.StatusCode;
+            int expStatusforposts = (int)Status—ode.Step1;
+            Assert.IsTrue(statuscodeposts == expStatusforposts, $"Status code is NOT {expStatusforposts}");
 
-            //string? contentType = allposts.TypeResponseBody;
-            //Assert.That(contentType.Contains("json"));
+            string? contentType = allposts.TypeResponseBody;
+            Assert.That(contentType.Contains("json"));
 
-            //var expectedsort = allposts.Data.OrderBy(x => x.Id);
-            //Assert.AreEqual(expectedsort, allposts.Data);
+            var expectedsort = allposts.Data.OrderBy(x => x.Id);
+            Assert.AreEqual(expectedsort, allposts.Data);
+            AqualityServices.Logger.Info($"Step 2: Send GET request to get post with id=99");
+            ResponseModel<PostModel> post99resp = APIAppRequest<PostModel>.GetData("99");
+            int statuscodepost99 = post99resp.StatusCode;
+            int expStatusforpost99 = (int)Status—ode.Step2;
+            Assert.IsTrue(statuscodepost99 == expStatusforpost99, $"Status code is NOT {expStatusforpost99}");
 
-            //ResponseModel<PostModel> post99resp = APIAppRequest<PostModel>.GetData("99");
-            //int statuscodepost99 = post99resp.StatusCode;
-            //int expStatusforpost99 = (int)Status—ode.Step2;
-            //Assert.IsTrue(statuscodepost99 == expStatusforpost99, $"Status code is NOT {expStatusforpost99}");
+            PostModel expectedpost99 = TestDataUtils<PostModel>.GetTestData("testDataPost99.json");
+            Assert.AreEqual(expectedpost99, post99resp.Data.First());
+            Assert.IsTrue(post99resp.Data[0].Equals(expectedpost99));
+            AqualityServices.Logger.Info($"Step 3: Send GET request to get post with id=150");
+            ResponseModel<PostModel> post150resp = APIAppRequest<PostModel>.GetData("150");
+            Assert.AreEqual((int)Status—ode.Step3, post150resp.StatusCode, $"Status code is NOT {(int)Status—ode.Step3}");
 
-            //PostModel expectedpost99 = TestDataUtils<PostModel>.GetTestData("testDataPost99.json");
-            //Assert.AreEqual(expectedpost99, post99resp.Data.First());
-            //Assert.IsTrue(post99resp.Data[0].Equals(expectedpost99));
-
-            //ResponseModel<PostModel> post150resp = APIAppRequest<PostModel>.GetData("150");
-            //Assert.AreEqual((int)Status—ode.Step3, post150resp.StatusCode, $"Status code is NOT {(int)Status—ode.Step3}");
-
-            //Assert.True(post150resp.Data.First().IsResponseBodyEmpty(), "Response body is NOT empty");
-
+            Assert.True(post150resp.Data.First().IsResponseBodyEmpty(), "Response body is NOT empty");
+            AqualityServices.Logger.Info("Step 4: Send POST request");
             PostModel newPost = TestDataUtils<PostModel>.GetTestData("testDataNewPost.json");
             ResponseModel<PostModel> newPostresp = APIAppRequest<PostModel>.PostData(newPost);
             int expStatusfornewPost = (int)Status—ode.Step4;
             Assert.AreEqual(expStatusfornewPost, newPostresp.StatusCode, $"Status code is NOT {expStatusfornewPost}");
             Assert.IsTrue(newPostresp.Data.First().Equals(newPost), "Post information is NOT correct or id is NOT present in response");
-
+            AqualityServices.Logger.Info("Step 5: Send GET request to get users");
             ResponseModel<UserModel> allusers = APIAppRequest<UserModel>.GetData("","/users/");
             int statuscodeusers = allusers.StatusCode;
             int expStatusforusers = (int)Status—ode.Step5;
             Assert.IsTrue(statuscodeusers == expStatusforusers, $"Status code is NOT {expStatusforusers}");
             Assert.That(allusers.TypeResponseBody.Contains("json"));
-
+            AqualityServices.Logger.Info($"Step 6: Send GET request to get user with id=5");
             UserModel expexteduser5 = TestDataUtils<UserModel>.GetTestData("testDataUser5.json");
             UserModel user5fromlist = allusers.Data.Find(p => p.Id == 5);
             Assert.That(expexteduser5.Equals(user5fromlist), "User (id=5) data NOT equals to test data");
