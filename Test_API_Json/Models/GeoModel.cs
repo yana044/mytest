930029e72eb9358c2b453d3843f38baa3bc3357d
yana044 <ya.natalya044@yandex.by>
2022-09-28 11:37:39 +0300

    add task

---
+++ Test_API_Json/Models/GeoModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_API_Json.Models
+{
+    internal class GeoModel
+    {
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            { return false; }
+            GeoModel geo = obj as GeoModel;
+            if (geo == null)
+            { return false; }
+
+            return Lat == geo.Lat && Lng == geo.Lng;
+        }
+    }
+}
---
+++ Test_API_Json/Utils/TestDataUtils.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_API_Json.Utils
+{
+    internal class TestDataUtils<T>
+    {
+        public static string GetConfigData(string key)
+        {
+            using StreamReader reader = File.OpenText("configData.json");
+            {
+                JsonTextReader jsonTextReader = new JsonTextReader(reader);
+                JObject jsonObject = (JObject)JToken.ReadFrom(jsonTextReader);
+                string value = jsonObject[key].ToString();
+                reader.Close();
+                return value;
+            }
+        }
+        public static T GetTestData(string filename)
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string json = reader.ReadToEnd();
+                T testdata = JsonConvert.DeserializeObject<T>(json);
+                reader.Close();
+                return testdata;
+            }
+
+        }
+        //public static T GetExpectedData(string key)
+        //{
+        //    using StreamReader reader = File.OpenText("configData.json");
+        //    {
+        //        JsonTextReader jsonTextReader = new JsonTextReader(reader);
+        //        JObject jsonObject = (JObject)JToken.ReadFrom(jsonTextReader);
+        //        T value = JsonConvert.DeserializeObject<T>(jsonObject[key].ToString());
+        //        reader.Close();
+        //        return value;
+        //    }
+        //}
+    }
+}
